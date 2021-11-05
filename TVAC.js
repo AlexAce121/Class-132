b7ffe09bf7c54e42fc12876450770875a1e8c0c6
AlexAce121 <82585887+AlexAce121@users.noreply.github.com>
2021-11-05 16:49:25 +0530

    Add files via upload

---
+++ TVAC.js
@@ -0,0 +1,30 @@
+function setup() {
+    canvas = createCanvas(640 , 420);
+    canvas.center();
+    objectDetector = ml5.objectDetector('cocossd' , modelLoaded);
+    document.getElementById("status").innerHTML = "Status: Detecting Objects" ;
+}
+
+img = "";
+statuses = "";
+
+function preload() {
+    img = loadImage('TVAC.jpg')
+}
+
+function modelLoaded() {
+    console.log('Model Loaded');
+    statuses = true;
+    objectDetector.detect(img , gotResult)
+}
+
+function gotResult(error , results) {
+    if (error) {
+       console.log(error); 
+    }
+    console.log(results);
+}
+
+function draw() {
+    image(img , 0 , 0 , 640 , 420);
+}
